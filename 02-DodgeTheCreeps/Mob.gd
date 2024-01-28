4a381250b50c7e9e1ae075c3327178018e096888
DanielSalis <danielsalis98@gmail.com>
2024-01-28 17:49:11 -0300

    chore: test

--- 02-DodgeTheCreeps/Mob.gd
+++ 02-DodgeTheCreeps/Mob.gd
@@ -6,6 +6,7 @@ export var max_speed = 250
 
 func _ready():
 	var mob_types = $AnimatedSprite.get_animation_names()
+	print(str(mob_types))
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
 	
 
